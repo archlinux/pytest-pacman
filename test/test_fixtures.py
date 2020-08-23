b3941ee30482874ff3cbabb90bc8598ef7589818
Jelle van der Waa <jelle@archlinux.org>
2020-08-23 22:06:37 +0200

    Add test for no provided path

--- test/test_fixtures.py
+++ test/test_fixtures.py
@@ -17,7 +17,12 @@ def core_data():
 
 
 @pytest.fixture
-def localdb(tmpdir_factory, core_data, generate_localdb):
+def localdb(core_data, generate_localdb):
+    return generate_localdb(core_data)
+
+
+@pytest.fixture
+def localdb_tmpdir(tmpdir_factory, core_data, generate_localdb):
     dbpath = str(tmpdir_factory.mktemp('dbpath'))
     return generate_localdb(core_data, dbpath)
 
@@ -26,4 +31,11 @@ def test_localdb(core_data, localdb):
     pkg = core_data[0]
     pkgpath = f"{localdb}/local/{pkg['name']}-{pkg['version']}"
     assert os.path.exists(localdb)
+    assert os.path.exists(pkgpath)
+
+
+def test_localdb_tmpdir(core_data, localdb_tmpdir):
+    pkg = core_data[0]
+    pkgpath = f"{localdb_tmpdir}/local/{pkg['name']}-{pkg['version']}"
+    assert os.path.exists(localdb_tmpdir)
     assert os.path.exists(pkgpath)
